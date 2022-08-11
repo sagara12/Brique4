c3f1e08d910d78c76a4362a4d5300b5749d268a3
sagara12 <sagara12@naver.com>
2022-08-11 10:53:59 +0900

    first commit

---
+++ Problem.js
@@ -0,0 +1,139 @@
+
+
+var tem  = [];
+var humi = [];
+var month = [];
+
+
+//온도 넣기
+function saveData1()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData2()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData3()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData4()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData5()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData5()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData6()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData7()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData8()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData9()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData10()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData11()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+function saveData12()  {
+    var value1 = document.getElementById('1md').value;
+    tem.push(value1)
+    month.push
+}
+
+
+//습도 넣기
+function saveDataH1()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+function saveDataH2()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+function saveDataH3()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+function saveDataH4()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+function saveDataH5()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+function saveDataH6()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+function saveDataH8()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+function saveDataH9()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+function saveDataH10()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+function saveDataH11()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+function saveDataH12()  {
+    var value1 = document.getElementById('1mh').value;
+    tem.push(value1)
+}
+
+
+
+
+
+
+
+
+
